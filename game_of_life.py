f4fc8fd673cc450cea4070de4fc6289befdf1bc5
Kyle Marsh <KMarsh@cs.hmc.edu>
2010-07-21 21:25:37 -0700

    Starting my game of life project!
    Wrote up a README with some initial direction.
    Wrote up game_of_life.py with "Hello World" type TkInter program.

---
+++ game_of_life.py
@@ -0,0 +1,10 @@
+'''
+Created on Jul 21, 2010
+@author: Kyle Marsh <KMarsh@cs.hmc.edu>
+'''
+import Tkinter
+
+if __name__ == '__main__':
+    l = Tkinter.Label(text = "Hello World")
+    l.pack()
+    l.mainloop()
